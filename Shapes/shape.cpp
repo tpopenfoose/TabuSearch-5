66b0f086349a5e213d5c129a686b1f0342d6bff6
Arkadiusz Damian <arkadiuszdamian@gmail.com>
2014-08-30 15:22:43 +0200

    modified Inserter test

--- Shapes/shape.cpp
+++ Shapes/shape.cpp
@@ -1,18 +1,13 @@
 #include "shape.h"
 
-Shape::Shape()
-{
-
-}
-
-Shape::~Shape()
-{
-
-}
-
 Shape::Shape(QColor color) :
      m_x(0), m_y(0), m_color(color)
 {
+}
+
+bool Shape::operator==(const Shape& s) const
+{
+    return s.m_x == m_x && s.m_y == m_y;
 }
 
 void Shape::set_x(int p_x)
--- tests/InserterTestSuite.cpp
+++ tests/InserterTestSuite.cpp
@@ -24,9 +24,28 @@ TEST_F(InserterTestSuite, shouldReturnProperShapseForGivenData)
     std::vector<std::shared_ptr<Shape>> result{shp1, shp2, shp3};
 
     ConfigData l_data(40,20,20,0,0,0,0);
-    Shapes l_expectedShapes = inserter.insert(result, l_data);
+    Shapes l_reveivedShapes = inserter.insert(result, l_data);
 
-    ASSERT_EQ(l_expectedShapes.second, 2);
+    std::shared_ptr<Shape> expectedShp1 = std::make_shared<Rectangle>();
+    expectedShp1->set_x(0);
+    expectedShp1->set_y(0);
+    std::shared_ptr<Shape> expectedShp2 = std::make_shared<Rectangle>();
+    expectedShp2->set_x(20);
+    expectedShp2->set_y(0);
+    std::shared_ptr<Shape> expectedShp3 = std::make_shared<Rectangle>();
+    expectedShp3->set_x(0);
+    expectedShp3->set_y(0);
+
+    Shapes l_expectedShapes{{expectedShp1, expectedShp2, expectedShp3}, 2};
+
+    for(auto iter1 = l_expectedShapes.first.begin(), iter2 = l_reveivedShapes.first.begin();
+        iter1 != l_expectedShapes.first.end() && iter2 != l_reveivedShapes.first.end();
+        ++iter1, ++iter2)
+    {
+        std::shared_ptr<Shape> s1 = *iter1;
+        std::shared_ptr<Shape> s2 = *iter2;
+        ASSERT_EQ(*s1, *s2);
+    }
 }
 
 } //namespace Ut
